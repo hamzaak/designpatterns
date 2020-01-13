58631351703bff3c313c9bbfb58ca9505d900b6f
Hamza Ak <ak.hamza.88@gmail.com>
2020-01-13 15:24:20 +0300

    factory method

---
+++ factorymethod/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+
+/*
+Factory Method is a Design Pattern which defines an interface for creating an object, 
+but lets the classes that implement the interface decide which class to instantiate. 
+Factory Pattern lets a class postpone instantiation to sub-classes.
+
+The factory pattern is used to replace class constructors, 
+abstracting the process of object generation so that the type of the object instantiated can be determined at run-time.
+*/
+
+namespace factorymethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardFactory factory = null;
+            Console.WriteLine("Enter the card type you would like to visit:");
+            string cardType = Console.ReadLine();
+            switch (cardType.ToLower())
+            {
+                case "moneyback":
+                    factory = new MoneyBackFactory(50000, 0);
+                    break;
+                case "titanium":
+                    factory = new TitaniumFactory(100000, 500);
+                    break;
+                case "platinum":
+                    factory = new PlatinumFactory(500000, 1000);
+                    break;
+                default:
+                    break;
+            }
+
+            CreditCard creditCard = factory.GetCreditCard();
+            Console.WriteLine("\nYour card details are below: \n");
+            Console.WriteLine($"Card Type: {creditCard.CardType}\nCredit Limit: {creditCard.CreditLimit}\nAnnual Charge: {creditCard.AnnualCharge}");
+            Console.ReadKey();
+        }
+    }
+
+    // 1. Product
+    abstract class CreditCard
+    {
+        public abstract string CardType { get; }
+        public abstract int CreditLimit { get; set; }
+        public abstract int AnnualCharge { get; set; }
+    }
+
+    // 2. ConcreteProducts
+    class MoneyBackCreditCard : CreditCard
+    {
+        private readonly string _cardType;
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public MoneyBackCreditCard(int creditLimit, int annualCharge)
+        {
+            _cardType = "MoneyBack";
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override string CardType
+        {
+            get { return _cardType; }
+        }
+
+        public override int CreditLimit
+        {
+            get { return _creditLimit; }
+            set { _creditLimit = value; }
+        }
+
+        public override int AnnualCharge
+        {
+            get { return _annualCharge; }
+            set { _annualCharge = value; }
+        }
+    }
+
+    class TitaniumCreditCard : CreditCard
+    {
+        private readonly string _cardType;
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public TitaniumCreditCard(int creditLimit, int annualCharge)
+        {
+            _cardType = "Titanium";
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override string CardType
+        {
+            get { return _cardType; }
+        }
+
+        public override int CreditLimit
+        {
+            get { return _creditLimit; }
+            set { _creditLimit = value; }
+        }
+
+        public override int AnnualCharge
+        {
+            get { return _annualCharge; }
+            set { _annualCharge = value; }
+        }
+    }
+
+    class PlatinumCreditCard : CreditCard
+    {
+        private readonly string _cardType;
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public PlatinumCreditCard(int creditLimit, int annualCharge)
+        {
+            _cardType = "Platinum";
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override string CardType
+        {
+            get { return _cardType; }
+        }
+
+        public override int CreditLimit
+        {
+            get { return _creditLimit; }
+            set { _creditLimit = value; }
+        }
+
+        public override int AnnualCharge
+        {
+            get { return _annualCharge; }
+            set { _annualCharge = value; }
+        }
+    }
+
+    // 3. Creator
+    abstract class CardFactory
+    {
+        public abstract CreditCard GetCreditCard();
+    }
+
+    // 4. ConcreteCreators
+    class MoneyBackFactory : CardFactory
+    {
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public MoneyBackFactory(int creditLimit, int annualCharge)
+        {
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override CreditCard GetCreditCard()
+        {
+            return new MoneyBackCreditCard(_creditLimit, _annualCharge);
+        }
+    }
+
+    class TitaniumFactory : CardFactory
+    {
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public TitaniumFactory(int creditLimit, int annualCharge)
+        {
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override CreditCard GetCreditCard()
+        {
+            return new TitaniumCreditCard(_creditLimit, _annualCharge);
+        }
+    }
+
+    class PlatinumFactory : CardFactory
+    {
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public PlatinumFactory(int creditLimit, int annualCharge)
+        {
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override CreditCard GetCreditCard()
+        {
+            return new PlatinumCreditCard(_creditLimit, _annualCharge);
+        }
+    }
+
+}
