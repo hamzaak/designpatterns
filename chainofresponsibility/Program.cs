4d45df54e6ddf6a2cd5a1d9dfe827234714e74c2
Hamza Ak <ak.hamza.88@gmail.com>
2020-01-16 10:33:43 +0300

    chainofresponsibility added

---
+++ chainofresponsibility/Program.cs
@@ -0,0 +1,131 @@
+﻿/*
+    > The chain of responsibility pattern is used to process a list or chain of various types of request 
+      and each of them may be handled by a different handler. 
+    > This pattern decouples sender and receiver of a request based on the type of request.
+    > In this pattern, normally each receiver (handler) contains a reference to another receiver. 
+      If one receiver cannot handle the request then it passes the same to the next receiver and so on.
+*/
+
+using System;
+
+namespace chainofresponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setup the chain of responsibility
+            var mustafa = new Manager();
+            var emre = new AssistantManager();
+            var hamza = new Clerk();
+
+            emre.Successor = mustafa;
+            hamza.Successor = emre;
+
+            // Make loan requests
+            var loan = new Loan { Number = 2034, Amount = 24000.00, Purpose = "Laptop Loan" };
+            hamza.ProcessRequest(loan);
+
+            loan = new Loan { Number = 2035, Amount = 42000.00, Purpose = "Bike Loan" };
+            hamza.ProcessRequest(loan);
+
+            loan = new Loan { Number = 2036, Amount = 72000.00, Purpose = "Car Loan" };
+            hamza.ProcessRequest(loan);
+
+            loan = new Loan { Number = 2037, Amount = 156200.00, Purpose = "House Loan" };
+            hamza.ProcessRequest(loan);
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class Loan
+    {
+        public double Amount { get; set; }
+        public string Purpose { get; set; }
+        public int Number { get; set; }
+    }
+
+    // Loan event argument holds Loan info
+    class LoanEventArgs : EventArgs
+    {
+        internal Loan Loan { get; set; }
+    }
+
+    abstract class Approver
+    {
+        public Approver Successor { get; set; }
+
+        public EventHandler<LoanEventArgs> Loan;
+        public abstract void LoanHandler(object sender, LoanEventArgs e);
+
+        public Approver()
+        {
+            Loan += LoanHandler;
+        }
+
+        public void ProcessRequest(Loan loan)
+        {
+            OnLoan(new LoanEventArgs { Loan = loan });
+        }
+
+        public virtual void OnLoan(LoanEventArgs e)
+        {
+            if (Loan != null)
+            {
+                Loan(this, e);
+            }
+        }
+
+    }
+
+    class Clerk : Approver
+    {
+        public override void LoanHandler(object sender, LoanEventArgs e)
+        {
+            if (e.Loan.Amount < 25000.0)
+            {
+                Console.WriteLine($"{this.GetType().Name} approved request# {e.Loan.Number}");
+            }
+            else if (Successor != null)
+            {
+                Successor.LoanHandler(this, e);
+            }
+        }
+    }
+
+    class AssistantManager : Approver
+    {
+        public override void LoanHandler(object sender, LoanEventArgs e)
+        {
+            if (e.Loan.Amount < 45000.0)
+            {
+                Console.WriteLine($"{this.GetType().Name} approved request# {e.Loan.Number}");
+            }
+            else if (Successor != null)
+            {
+                Successor.LoanHandler(this, e);
+            }
+        }
+    }
+
+    class Manager : Approver
+    {
+        public override void LoanHandler(object sender, LoanEventArgs e)
+        {
+            if (e.Loan.Amount < 100000.0)
+            {
+                Console.WriteLine($"{this.GetType().Name} approved request# {e.Loan.Number}");
+            }
+            else if (Successor != null)
+            {
+                Successor.LoanHandler(this, e);
+            }
+            else
+            {
+                Console.WriteLine($"Request# {e.Loan.Number} requires an executive meeting!");
+            }
+        }
+    }
+}
